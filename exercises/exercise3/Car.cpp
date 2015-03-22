f1605f5df595d8feee5d7f8e524f387ae9b1d22b
nkvelkov <nkvelkov>
2015-03-22 18:47:24 +0200

    Added solutions to the problems of the fourth exercise

---
+++ exercises/exercise3/Car.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Car
+{
+public:
+    Car()
+    {
+        car = 0;
+        price = 0.0;
+        strcpy(model," ");
+    }
+
+    Car(int year, const char* _model, double price)
+    {
+        this->year = year;
+        this->price = price;
+        strcpy(model, _model);
+    }
+
+    void setModel(const char* _model)
+    {
+        strcpy(model, _model);
+    }
+
+    void setYear(int _year)
+    {
+        year = _year;
+    }
+
+    void setPrice(double _price)
+    {
+        price = _price;
+    }
+
+    const char* getModel() const
+    {
+        return model;
+    }
+
+    int getYear() const
+    {
+        return year;
+    }
+
+    double getPrice() const
+    {
+        return price;
+    }
+
+    void print() const
+    {
+        cout << "Model: " << model << endl
+             << "Year: "  << year  << endl
+             << "Price: " << price << endl;
+    }
+
+private:
+    int year;
+    char model[51];
+    double price;
+};
+
+Car mostExpensiveCar (Car* cars, int n)
+{
+    Car maxPriceCar = cars[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (cars[i].getPrice() > maxPriceCar.getPrice())
+        {
+            maxPriceCar = cars[i];
+        }
+    }
+
+    return maxPriceCar;
+}
+
+int main ()
+{
+    Car d(1960,"Trabant",200);
+    Car a(1961,"Shkoda", 300);
+    Car b(2012,"Trabant 3000", 500);
+
+    Car c[3] = {d, a, b};
+
+    cout << "Most expensive car:\n";
+    mostExpensiveCar(c,3).print();
+    cout << endl << endl;
+
+    d.print();
+    d.setModel("tr");
+    d.print();
+
+    return 0;
+}
---
+++ exercises/exercise3/SmartPhone.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Smartphone
+{
+public:
+    Smartphone(const char* _model="",
+               double price=0.0,
+               const char* _os="symbian")
+    {
+        this->price = price;
+        strcpy(model, _model);
+        strcpy(os, _os);
+    }
+
+    void setModel(char* _model)
+    {
+        strcpy(model, _model);
+    }
+    void setPrice(double _price)
+    {
+        price = _price;
+    }
+
+    void setOs(char* _os)
+    {
+        strcpy(os, _os);
+    }
+
+    const char* getModel() const
+    {
+        return model;
+    }
+    double getPrice() const
+    {
+        return price;
+    }
+
+    const char* getOs() const
+    {
+        return os;
+    }
+
+    void print() const
+    {
+         cout << "Model: " << model << endl
+              << "Price: " << price << endl
+              << "OS: "    << os    << endl;
+    }
+
+private:
+    double price;
+    char model[21];
+    char os[21];
+};
+
+void sortByPrice (Smartphone* s, int n)
+{
+    for (int i = 0; i < n - 1; i ++)
+    {
+        int min = s[i].getPrice();
+        int index = i;
+
+        for (int j = i + 1; j < n; j++)
+            if (s[j].getPrice() < min)
+            {
+                index = j;
+                min = s[j].getPrice();
+            }
+
+        if (index != i)
+        {
+            Smartphone temp = s[i];
+            s[i] = s[index];
+            s[index] = temp;
+        }
+    }
+}
+int main ()
+{
+    Smartphone n("Nokia",650,"windows");
+    Smartphone s("Samsung", 500, "Android");
+    Smartphone i("iPhone",700,"iOS");
+
+    Smartphone t[3] = {n,s,i};
+
+    sortByPrice(t,3);
+
+    cout << "After sort" << endl;
+
+
+
+    for (int k = 0; k < 3; k++)
+        t[k].print();
+
+    return 0;
+}
